846036bd66db2c4544c26308325aa99191770bf0
Adheip Singh <adheip1222@gmail.com>
2022-05-05 02:19:24 +0530

    http interface

--- cmd/http.go
+++ cmd/http.go
@@ -12,14 +12,34 @@ func parseableStreamURL(streamName string) string {
 	return os.Getenv("PARSEABLE_URL") + "/api/v1/stream/" + streamName
 }
 
-func httpPost(logs []byte, labels map[string]string, url string) error {
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(logs))
+type httpParseable interface {
+	doHttpRequest() error
+}
+
+// httpRequest holds all the fields needed for a HTTP request
+// to parseable server.
+type httpRequest struct {
+	method string
+	url    string
+	labels map[string]string
+	body   []byte
+}
+
+func newHttpRequest(method, url string, labels map[string]string, body []byte) *httpRequest {
+	return &httpRequest{method: method, url: url, labels: labels, body: body}
+}
+
+func (h *httpRequest) doHttpRequest() error {
+	req, err := http.NewRequest(h.method, h.url, bytes.NewBuffer(h.body))
 	if err != nil {
 		return err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	for key, value := range labels {
-		req.Header.Add(METADATA_LABEL+key, value)
+
+	if h.labels != nil {
+		for key, value := range h.labels {
+			req.Header.Add(METADATA_LABEL+key, value)
+		}
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -29,16 +49,47 @@ func httpPost(logs []byte, labels map[string]string, url string) error {
 	return nil
 }
 
-func httpPut(url string) error {
-	req, err := http.NewRequest("PUT", url, nil)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	return nil
-}
+// func httpPost(logs []byte, labels map[string]string, url string) error {
+// 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(logs))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req.Header.Set("Content-Type", "application/json")
+// 	for key, value := range labels {
+// 		req.Header.Add(METADATA_LABEL+key, value)
+// 	}
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+// 	return nil
+// }
+
+// func httpPut(url string) error {
+// 	req, err := http.NewRequest("PUT", url, nil)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req.Header.Set("Content-Type", "application/json")
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+// 	return nil
+// }
+
+// func httpGet(url string) error {
+// 	req, err := http.NewRequest("GET", url, nil)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req.Header.Set("Content-Type", "application/json")
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+// 	return nil
+// }
